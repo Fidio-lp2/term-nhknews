e5b0566a01991281e5c05f10622406b0de738a4f
Fidio-lp2 <qdatian9@gmail.com>
2021-08-06 20:57:49 +0900

    middle of working

--- term-nhknews/test.py
+++ term-nhknews/test.py
@@ -1,3 +1,14 @@
 # -*- encoding:utf-8 -*-
 
+import feedparser
+import pprint
 
+
+RSS_URL = 'https://www.nhk.or.jp/rss/news/cat0.xml'
+
+d = feedparser.parse(RSS_URL)
+pprint.pprint(d)
+
+print(d.get('feed').get('subtitle'))
+# for entry in d.entries:
+#     print(entry.title, entry.link)
