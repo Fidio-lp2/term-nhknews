1700023c54ff90557e8e2689279890c76378f774
Fidio-lp2 <qdatian9@gmail.com>
2021-08-12 18:53:34 +0900

    微調整

--- term-nhknews/__main__.py
+++ term-nhknews/__main__.py
@@ -14,25 +14,26 @@ from util import inves_app_path
 from color import *
 
 
-datapath: str = inves_app_path() + "/urldata.json"
-with open(datapath, 'r') as dataFile:
-    URLDATA: Final[Dict[str, str]] = json.load(dataFile)
+def main():
+    datapath: str = inves_app_path() + "/urldata.json"
+    with open(datapath, 'r') as dataFile:
+        URLDATA: Final[Dict[str, str]] = json.load(dataFile)
 
-parser = argparse.ArgumentParser(
-        description="If you use this app, \
-        you can know easily social situation on command line!!"
-)
+    parser = argparse.ArgumentParser(
+            description="If you use this app, \
+            you can know easily social situation on command line!!"
+    )
 
-parser.add_argument('-t', '--type', default="main",
-                    help="You can choose the news type which you want.\
-                    can choose 'main', 'society', 'chemotherapy', 'politics',\
-                    'economy', 'international', 'sports', 'culture', 'live'."
-                    )
+    parser.add_argument('-t', '--type', default="main",
+                        help="You can choose the news type which you want.\
+                        can choose 'main', 'society', 'chemotherapy', 'politics',\
+                        'economy', 'international', 'sports', 'culture', 'live'."
+                        )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-feed = feedparser.parse(URLDATA[args.type])
-print(feed["feed"]["title"] + ' ' + \
-        '[' + args.type + ']' + ' ' + feed["feed"]["updated"])
-for entry in feed.entries:
-    print(entry.title + '\n' + entry.summary + '\n' + entry.link)
+    feed = feedparser.parse(URLDATA[args.type])
+    print(feed["feed"]["title"] + ' ' + \
+            '[' + args.type + ']' + ' ' + feed["feed"]["updated"])
+    for entry in feed.entries:
+        print(entry.title + '\n' )#+ entry.summary + '\n' + entry.link)
